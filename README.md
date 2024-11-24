c8701c2dfc71a713291eb0830213ca33a75258e3
André Luiz <154485939+andreluizdasilvaa@users.noreply.github.com>
2024-11-24 15:06:29 -0300

    end

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Dead Pixel Detector
- Um projeto feito em 2 minutos para detectar um "pixel morto" em um tela
+# DeadPixel Detector
+
+## 📋 Descrição
+
+DeadPixel Detector é uma ferramenta simples para detectar pixels mortos ou travados em monitores e telas. Com um clique, a página muda de cor, ajudando a identificar rapidamente falhas na exibição.
+
+---
+
+## ✨ Funcionalidades
+
+-   Troca de cores do fundo ao clicar na tela.
+-   Cores sólidas e contrastantes (preto, branco, vermelho, verde, azul, roxo) para facilitar a visualização de pixels defeituosos.
+-   Interface limpa e minimalista para foco total na detecção.
---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const bd = document.querySelector("html");
+    const colors = ["red", "green", "blue", "white", "black", "purple"];
+
+    var num = 0;
+    bd.addEventListener("click", () => {
+        document.documentElement.requestFullscreen()
+        if (num > 5) {
+            num = 0;
+        }
+        const cor = colors[num];
+
+        bd.style.backgroundColor = cor;
+        num++;
+    });
+
+    bd.addEventListener("mouseenter", () => {
+        bd.style.cursor = "none";
+    });
+
+    bd.addEventListener("mouseleave", () => {
+        bd.style.cursor = "default";
+    });
+});
